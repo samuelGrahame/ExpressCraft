f9b0261669044b8c7d29eb0bfa679e0eacd75b9f
samuel grahame <samuel@intelogy.com.au>
2017-01-20 14:29:29 +1100

    Change

---
+++ ExpressCraft/forms/DialogForm.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Bridge;
+using Bridge.Html5;
+using ExpressCraft;
+
+namespace ExpressCraft
+{
+	public class DialogForm : Form
+	{
+		protected List<SimpleDialogButton> _buttonCollection;
+
+		public HTMLDivElement ButtonSection;
+		
+		public DialogForm(string text = "") : base()
+		{
+			this.Text = text;
+			ButtonSection = Div("dialogbuttonsection");
+
+			this.Body.AppendChild(ButtonSection);
+		}
+	}
+}
