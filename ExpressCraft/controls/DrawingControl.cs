e9e51e86dfe94a7091d6eed887fd9296dcef47e9
samuel grahame <samuel@intelogy.com.au>
2017-09-19 16:39:43 +1000

    DrawingControl - Support Touch Events

--- ExpressCraft/controls/DrawingControl.cs
+++ ExpressCraft/controls/DrawingControl.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Bridge.Html5;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,37 +18,74 @@ namespace ExpressCraft
         {
             ClassList.Add("inputcontrol");
             Style.BackgroundColor = "white";
-
-            Content.OnMouseDown = (ev) =>
+            
+            if(Helper.NotDesktop)
             {
-                __mouseDown = true;
-                __currentCursor = new Vector2(ev.LayerX, ev.LayerY);
-                
-                OnPaint();
-            };
-            Content.OnMouseMove = (ev) =>
-            {
-                if(__mouseDown)
+                Content.OnTouchStart = (ev) =>
                 {
-                    var np = new Vector2(ev.LayerX, ev.LayerY);
-                    Lines.Add(new Vector4(__currentCursor, np));
-                    __currentCursor = np;
+                    __mouseDown = true;
+                    __currentCursor = new Vector2(ev.LayerX, ev.LayerY);
 
                     OnPaint();
-                }                
-            };
-            Content.OnMouseUp = (ev) =>
-            {
-                var np = new Vector2(ev.LayerX, ev.LayerY);
-                if(np.Xi != __currentCursor.Xi || np.Yi != __currentCursor.Yi)
+                };
+                Content.OnTouchMove = (ev) =>
                 {
-                    Lines.Add(new Vector4(__currentCursor, np));
-                }                               
-                __mouseDown = false;
+                    if(__mouseDown)
+                    {
+                        var np = new Vector2(ev.LayerX, ev.LayerY);
+                        Lines.Add(new Vector4(__currentCursor, np));
+                        __currentCursor = np;
 
-                OnPaint();
-            };
-        }
+                        OnPaint();
+                    }
+                };
+                Content.OnTouchEnd = (ev) =>
+                {
+                    var np = new Vector2(ev.LayerX, ev.LayerY);
+                    if(np.Xi != __currentCursor.Xi || np.Yi != __currentCursor.Yi)
+                    {
+                        Lines.Add(new Vector4(__currentCursor, np));
+                    }
+                    __mouseDown = false;
+
+                    OnPaint();
+                }; 
+            }
+            else
+            {
+                Content.OnMouseDown = (ev) =>
+                {
+                    __mouseDown = true;
+                    __currentCursor = new Vector2(ev.LayerX, ev.LayerY);
+
+                    OnPaint();
+                };
+                Content.OnMouseMove = (ev) =>
+                {
+                    if(__mouseDown)
+                    {
+                        var np = new Vector2(ev.LayerX, ev.LayerY);
+                        Lines.Add(new Vector4(__currentCursor, np));
+                        __currentCursor = np;
+
+                        OnPaint();
+                    }
+                };
+                Content.OnMouseUp = (ev) =>
+                {
+                    var np = new Vector2(ev.LayerX, ev.LayerY);
+                    if(np.Xi != __currentCursor.Xi || np.Yi != __currentCursor.Yi)
+                    {
+                        Lines.Add(new Vector4(__currentCursor, np));
+                    }
+                    __mouseDown = false;
+
+                    OnPaint();
+                }; 
+            }
+
+            
+        }        
 
         public override void OnClear()
         {
