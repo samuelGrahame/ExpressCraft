3a92019da32ba6721e43fdbdc0027a212d6691fb
samfromdeath <samuel@intelogy.com.au>
2017-11-18 18:56:58 +1100

    Added - Style using base64 + header at start

--- ExpressCraft/App.cs
+++ ExpressCraft/App.cs
@@ -7,8 +7,9 @@ namespace ExpressCraft
     {
         public static void Main()
         {
+            Document.Head.AppendChild(new HTMLLinkElement() { Rel = "Stylesheet", Type = "text/css", Href = "data:text/css;base64," + Settings.ExpressCraftCssBase64 });
             Settings.Setup();
-
+            
             var frm = new Form()  { Size = new Vector2(500, 500), StartPosition = FormStartPosition.Center };
 
             var x = new Test();
@@ -27,11 +28,11 @@ namespace ExpressCraft
             {
                 var g = CreateGraphics();
                 var rnd = new Random();
-                for(int i = 0; i < 10; i++)
+                for(int i = 0; i < 100; i++)
                 {                    
                     g.DrawLine(new Pen(Color.FromArgb(rnd.Next())), rnd.Next(10, 300), rnd.Next(10, 300), rnd.Next(10, 300), rnd.Next(10, 300));
                 }
-                
+                g.DrawString("Hello World", new Font("Arial 10pt"), new SolidBrush(Color.Black), 50, 50);
 
                 base.OnPaint();
             }
