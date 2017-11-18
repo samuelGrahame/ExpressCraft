f3c25763a24d94aef4d8d9687dce0b9e585ef6c3
samfromdeath <samuel@intelogy.com.au>
2017-11-18 20:19:10 +1100

    Added Id to ExpressCraft css link

--- ExpressCraft/App.cs
+++ ExpressCraft/App.cs
@@ -7,43 +7,43 @@ namespace ExpressCraft
     {
         public static void Main()
         {
-            Document.Head.AppendChild(new HTMLLinkElement() { Rel = "Stylesheet", Type = "text/css", Href = "data:text/css;base64," + Settings.ExpressCraftCssBase64 });
+            Document.Head.AppendChild(new HTMLLinkElement() { Id = "ExpressCraft", Rel = "Stylesheet", Type = "text/css", Href = "data:text/css;base64," + Settings.ExpressCraftCssBase64 });
             Settings.Setup();
             
-            var frm = new Form()  { Size = new Vector2(500, 500), StartPosition = FormStartPosition.Center };
+            //var frm = new Form()  { Size = new Vector2(500, 500), StartPosition = FormStartPosition.Center };
 
-            var x = new Test();
-            x.SetBoundsFull();
+            //var x = new Test();
+            //x.SetBoundsFull();
             
-            frm.AppendChild(x);
+            //frm.AppendChild(x);
 
-            frm.LinkchildToForm(x);
+            //frm.LinkchildToForm(x);
 
-            frm.Show();
+            //frm.Show();
         }
 
-        public class Test : CanvasControl
-        {
-            public override void OnPaint()
-            {
-                var g = CreateGraphics();
-                var rnd = new Random();
-                for(int i = 0; i < 100; i++)
-                {                    
-                    g.DrawLine(new Pen(Color.FromArgb(rnd.Next())), rnd.Next(10, 300), rnd.Next(10, 300), rnd.Next(10, 300), rnd.Next(10, 300));
-                }
-                g.DrawString("Hello World", new Font("Arial 10pt"), new SolidBrush(Color.Black), 50, 50);
+        //public class Test : CanvasControl
+        //{
+        //    public override void OnPaint()
+        //    {
+        //        var g = CreateGraphics();
+        //        var rnd = new Random();
+        //        for(int i = 0; i < 100; i++)
+        //        {                    
+        //            g.DrawLine(new Pen(Color.FromArgb(rnd.Next())), rnd.Next(10, 300), rnd.Next(10, 300), rnd.Next(10, 300), rnd.Next(10, 300));
+        //        }
+        //        g.DrawString("Hello World", new Font("Arial 10pt"), new SolidBrush(Color.Black), 50, 50);
 
-                g.DrawEllipse(new Pen(Color.Red, 2), 100, 100, 50, 50);
+        //        g.DrawEllipse(new Pen(Color.Red, 2), 100, 100, 50, 50);
 
-                g.FillEllipse(new SolidBrush(Color.Green), 100, 200, 50, 50);
+        //        g.FillEllipse(new SolidBrush(Color.Green), 100, 200, 50, 50);
 
-                var size = g.MeasureString("Hello World", new Font("Arial 11pt"));
+        //        var size = g.MeasureString("Hello World", new Font("Arial 11pt"));
 
-                g.DrawString(size.ToString(), new Font("Arial 11pt"), new SolidBrush(Color.Black), 200, 50);
+        //        g.DrawString(size.ToString(), new Font("Arial 11pt"), new SolidBrush(Color.Black), 200, 50);
 
-                base.OnPaint();
-            }
-        }
+        //        base.OnPaint();
+        //    }
+        //}
     }
 }
